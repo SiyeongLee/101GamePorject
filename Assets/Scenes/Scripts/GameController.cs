6071acb91c2870d16a02d31289eb67097942b93a
SiyeongLEE <happyamong123@naver.com>
2025-03-19 16:29:25 +0900

    [Feat]캐릭터 체력 변수 설정 및 캐릭터 생성 및 소멸

--- Assets/Scenes/Scripts/GameController.cs
+++ Assets/Scenes/Scripts/GameController.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    public float GameTimer = 0.0f;
+    public GameObject MonsterGo;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        GameTimer -= Time.deltaTime;
+        if (GameTimer <= 0)
+        {
+            GameTimer = 3.0f;
+
+            GameObject Temp = Instantiate(MonsterGo);
+            Temp.transform.position = new Vector3(Random.Range(-10, 10), Random.Range(-4, 4), 0.0f);
+
+        }
         if (Input.GetMouseButtonDown(0))// 마우스 버튼을 누르면
         {
             RaycastHit hit; //Ray 선언
@@ -22,7 +33,9 @@ public class GameController : MonoBehaviour
             {
                 if (hit.collider != null) //Hit 된 오브젝트가 있을경우
                 {
-                    Debug.Log
-
+                    hit.collider.gameObject.GetComponent<Monster>().CharacterHit(50);
                 }
+            }
+        }
+    }
 }
--- Assets/Scenes/Scripts/Monster.cs
+++ Assets/Scenes/Scripts/Monster.cs
@@ -11,14 +11,18 @@ public class Monster : MonoBehaviour
     // 첫 프레임 이전에 한번 실행 된다.
     void Start()
     {
-        Health += 100; //첫 프레임 이전에 실행될때 100체력을 추가 시켜준다.
+        Health = 100; //첫 프레임 이전에 실행될때 100체력을 추가 시켜준다.
     }
 
     // 매번 프레임 때 호출된다
     void Update()
     {
+        CharacterHealthUp();
+        CheckDeath();       //함수 호출
+    }
 
-        void CharacterHealthUp()
+    public void CharacterHealthUp()
+    {
         Timer -= Time.deltaTime; // 시간을 매 프레임마다 감소 시킨다.(deltaTime 프레임간의 시간 간격을 의미한다.)
 
         if (Timer < 0)//만약 TImer의 수치가 0이하로 내려갈 경우    }
@@ -26,18 +30,10 @@ public class Monster : MonoBehaviour
             Timer = 1.0f; //다시 1초로 변경 시켜 준다.
             Health += 20; // 1초마다 체력 20을 올려준다
         }
-
-        CharacterHealthUp();
-        if (Input.GetKeyUp(KeyCode.Space)) //스페이스 키를 눌렀을 때 
-        {
-            CharacterHit(AttackPoint);  //Health -= AttackPoint; //체력 포인트를 공격 포인트 만큼 감소 시켜준다. (Health = Health - AttackPoint)ss
-        }
-        CheckDeath();//함수 호출
-
-        if (Health <= 0) // 체력이 0 이하로 내려가면 파괴 시킨다.
-            Destroy(gameObject);
     }
-        public svoid CharacterHit(int Damage)//커스텀 데미지를 받는 함수를 사용한다.
+
+
+    public void CharacterHit(int Damage)//커스텀 데미지를 받는 함수를 사용한다.
     {
         Health -= Damage; //받은 공격력에 대한 체력을 감소 시킨다.
     }
